a65deb20550d66a3ec278de701033b71c7e8f294
ChenSingYo <chensingyo@gmail.com>
2023-10-08 14:29:26 +0800

    implement ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
+
+import { ThemeProvider } from '@/context/ThemeProvider'
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from 'next/font/google'
 import React from 'react'
 import './globals.css'
@@ -31,19 +34,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink:'primary-text-gradient hover:text-primary-500'
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500',
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
